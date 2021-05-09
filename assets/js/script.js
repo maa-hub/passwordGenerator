662d9cb119dba75252c5ac01b1da4641eac52cf3
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-05-09 01:24:02 -0400

    all password criteria options and console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,33 +21,94 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 var sizePassword = prompt("How many characters would you like your password? Choose between 8 and 128");
-console.log("Password is " + passwordLength + " long");
+console.log("Password is " + sizePassword + " long");
 
-while(!sizePassword || sizePassword < 8 || sizePassword > 128) {
+if(!sizePassword || sizePassword < 8 || sizePassword > 128) {
     sizePassword = prompt("Please enter a number between 8 and 128");
-    console.log("Password is " + passwordLength + " long");
-}
-    var confirmLower = confirm("Will your password contain a lower case letter?");
-    var confirmUpper = confirm("Will your password contain an upper case letter?");
-    var confirmSpecial = confirm("Will your pass contain a special character?");
-    var confirmNumber = confirm("Will your password contain a number?");
-console.log("Lowercase" + confirmLower);
-console.log("Uppercase" + confirmUpper);
-console.log("specialCharacter" + confirmSpecial);
-console.log("NUmber" + confirmNumber);
+    console.log("Password is " + sizePassword + " long");
 
+var sizePassword = prompt("How many characters would you like your password? Choose between 8 and 128");
 
-while (!(confirmUpper|| confirmLower || confirmNumber || confirmNumber)) {
-    alert("You must select at least one character type!");
-    confirmUpper = confirm("Would you like to use uppercase letters?");
-    confirmLower = confirm("Would you like to use lowercase letters?");
-    confirmNumber = confirm("would you like to use numbers?");
-    confirmNumber = confirm("would you like to use special characters?");
-  }
+} else {
+    var confirmUpper = confirm("Would you like to use uppercase letters?");
+    console.log(confirmUpper);
+    var confirmLower = confirm("Would you like to use lowercase letters?");
+    console.log(confirmLower);
+    var confirmNumber = confirm("would you like to use numbers?");
+    console.log(confirmNumber);
+    var confirmSpecial = confirm("would you like to use special characters?");
+    console.log(confirmSpecial);
+};
 
+ if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
+    feedback = alert("You must select at least one criteria");
+ }
+// all four criteria selected
 if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
-    feedback = lower.concat(upper, numbers, specialCharacters);
+    var feedback = lower.concat(upper, numbers, specialCharacters);
     console.log(feedback);
 }
-}
+
+// three criteria selected
+    else if (confirmLower && confirmUpper && confirmNumber) {
+        var feedback = lower.concat(upper, numbers);
+        console.log(feedback);
+      }
+      else if (confirmLower && confirmUpper && confirmSpecial) {
+        var feedback = lower.concat(upper, specialCharacters);
+        console.log(feedback);
+      }
+      else if (confirmLower && confirmNumber && confirmSpecial) {
+        var feedback = lower.concat(numbers, specialCharacters);
+        console.log(feedback);
+      }
+      else if (confirmUpper && confirmNumber && confirmSpecial) {
+        var feedback = upper.concat(numbers, specialCharacters);
+        console.log(feedback);
+      }
+
+      // two criteria selected
+      else if (confirmLower && confirmUpper) {
+        var feedback = lower.concat(upper);
+        console.log(feedback);
+      }
+      else if (confirmLower && confirmNumber) {
+        var feedback = lower.concat(numbers);
+        console.log(feedback);
+      }
+      else if (confirmLower && confirmSpecial) {
+        var feedback = lower.concat(specialCharacters);
+        console.log(feedback);
+      }
+      else if (confirmUpper && confirmNumber) {
+        var feedback = upper.concat(numbers);
+        console.log(feedback);
+      }
+      else if (confirmUpper && confirmSpecial) {
+        var feedback = upper.concat(specialCharacters);
+        console.log(feedback);
+      }
+      else if (confirmNumber && confirmSpecial) {
+        var feedback = numbers.concat(specialCharacters);
+        console.log(feedback);
+      }
+
+      // one criteria selected
+      else if (confirmLower) {
+        var feedback = lower;
+        console.log(feedback);
+      }
+      else if (confirmUpper) {
+        var feedback = upper;
+        console.log(feedback);
+      }
+      else if (confirmNumber) {
+        var feedback = numbers;
+        console.log(feedback);
+      }
+      else if (confirmSpecial) {
+        var feedback = specialCharacters;
+        console.log(feedback);
+      };
+
 
